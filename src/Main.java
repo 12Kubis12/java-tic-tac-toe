51e7a0395b6f49ed4d021bdf0affaff383264a1a
Jakub Pajak <12j.pajak12@gmail.com>
2025-04-15 19:40:16 +0200

    comments

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,61 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = 4;
+        String[][] markedPlaces = new String[size][size];
+
+        String winner = "";
+        String currentPlayer = "O";
+        int row;
+        int column;
+        int count = 0;
+
+        System.out.println("Here is the playing field:");
+        showPlayingField(markedPlaces);
+        System.out.println("Player 'O' goes first and player 'X' goes second.");
+//        Start the game.
+        while (winner.isEmpty()) {
+            System.out.println("Give me the position (numbers from 1 to " + size + "), where you want put your symbol (" +
+                    currentPlayer + ") -> example for first row and third column: 1;3");
+//            Check input.
+            try {
+                String[] position = scanner.nextLine().replaceAll("\\s", "").split(";");
+                row = (Integer.parseInt(position[0]) - 1);
+                column = (Integer.parseInt(position[1]) - 1);
+                if (markedPlaces[row][column] == null) {
+                    markedPlaces[row][column] = currentPlayer;
+                } else {
+                    System.out.println("The place is taken, look at the playing field and try again!!!");
+                    showPlayingField(markedPlaces);
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input, try again!!!");
+                continue;
+            }
+
+            showPlayingField(markedPlaces);
+//            Check winner.
+            if (checkWinner(markedPlaces)) {
+                winner = currentPlayer;
+                System.out.println("Player '" + currentPlayer + "' wins!!!");
+            } else if (currentPlayer.equals("O")) {
+                currentPlayer = "X";
+                count++;
+            } else {
+                currentPlayer = "O";
+                count++;
+            }
+//            Check draw.
+            if (count == Math.pow(size, 2)) {
+                System.out.println("Draw!!!");
+                break;
+            }
+        }
+    }
+
     public static void showPlayingField(String[][] markedPlaces) {
         String spacer = "-";
         int columnSize = markedPlaces.length;
@@ -50,59 +105,5 @@ public class Main {
             }
         }
         return winner;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int size = 4;
-        String[][] markedPlaces = new String[size][size];
-
-        String winner = "";
-        String currentPlayer = "O";
-        int row;
-        int column;
-        int count = 0;
-
-        System.out.println("Here is the playing field:");
-        showPlayingField(markedPlaces);
-        System.out.println("Player 'O' goes first and player 'X' goes second.");
-
-        while (winner.isEmpty()) {
-            System.out.println("Give me the position (numbers from 1 to " + size + "), where you want put your symbol (" +
-                    currentPlayer + ") -> example for first row and third column: 1;3");
-            try {
-                String[] position = scanner.nextLine().replaceAll("\\s", "").split(";");
-                row = (Integer.parseInt(position[0]) - 1);
-                column = (Integer.parseInt(position[1]) - 1);
-                if (markedPlaces[row][column] == null) {
-                    markedPlaces[row][column] = currentPlayer;
-                } else {
-                    System.out.println("The place is taken, look at the playing field and try again!!!");
-                    showPlayingField(markedPlaces);
-                    continue;
-                }
-            } catch (Exception e) {
-                System.out.println("Invalid input, try again!!!");
-                continue;
-            }
-
-            showPlayingField(markedPlaces);
-
-            if (checkWinner(markedPlaces)) {
-                winner = currentPlayer;
-                System.out.println("Player '" + currentPlayer + "' wins!!!");
-            } else if (currentPlayer.equals("O")) {
-                currentPlayer = "X";
-                count++;
-            } else {
-                currentPlayer = "O";
-                count++;
-            }
-
-            if (count == Math.pow(size, 2)) {
-                System.out.println("Draw!!!");
-                break;
-            }
-        }
     }
 }
